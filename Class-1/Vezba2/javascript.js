e2a82f6d3ada631fc157f5c4ebd501676d773991
Ertan Muslic <ertanmuslic@gmail.com>
2022-10-20 23:51:54 +0200

    First Commit

---
+++ Class-1/Vezba2/javascript.js
@@ -0,0 +1,22 @@
+const alertFn = () => {
+    alert("Welcome to my world!");
+}
+
+const changeColor = () => {
+    const par = document.getElementById("par1");
+    par.style.color = "red";
+    setTimeout(() => {
+        par.style.color = "orange";
+    }, 1000)
+}
+
+const changeImage = () => {
+    const img = document.getElementById("wallpaper");
+    img.src = "images/wallhaven-2.jpg"
+    setTimeout(() => {
+        img.src = "images/wallhaven-1.jpg"
+    }, 1000)
+}
+
+const par = document.getElementById("par1");
+par.style.color = "orange";
